0d3e9a8929f1784c3e5fc4e5c954e99d26cf73ed
Ethan Sagin <ethansagin@gmail.com>
2019-06-01 18:48:14 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -12,9 +12,10 @@ class Application
         resp.write "#{item}\n"
       end
     elsif req.path.match(/cart/)
-      @@cart.each do |item|
-        resp.write "#{item}\n"
-      end
+      
+        @@cart.each do |item|
+          resp.write "#{item}\n"
+        end
     elsif req.path.match(/search/)
       search_term = req.params["q"]
       resp.write handle_search(search_term)
